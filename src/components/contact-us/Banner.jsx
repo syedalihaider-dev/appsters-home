11149d7c63e4c32e5bcb2940b19326c8e655ad4d
syedalihaider-dev <aleehaiderbalti@gmail.com>
2026-05-08 04:36:00 +0500

    Import React UseState

--- src/components/contact-us/Banner.jsx
+++ src/components/contact-us/Banner.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import styles from './Banner.module.css';
