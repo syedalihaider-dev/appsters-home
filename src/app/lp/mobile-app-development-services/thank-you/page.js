cbd85d1af3192ffe9925d5d42f72bf763dcd16e2
syedalihaider-dev <aleehaiderbalti@gmail.com>
2026-05-20 02:33:27 +0500

    Remove Extra Button from Banner

--- src/app/lp/mobile-app-development-services/thank-you/page.js
+++ src/app/lp/mobile-app-development-services/thank-you/page.js
@@ -61,12 +61,6 @@ export default function ThankYouPage() {
                                 <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </Link>
-                        <a href="javascript:window.replaceChat();" className={styles.chatBtn}>
-                            Chat With Strategist
-                            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg>
-                        </a>
                     </div>
                 </div>
             </main>
